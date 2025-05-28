4421910aed77cb348447cf60223090647aac3bc1
Yaseenkhan006 <24pwbcs1391@uetpeshawar.edu.pk>
2025-05-28 19:36:59 +0500

    Update Snakegame.java
    Initial project setup

--- Snakegame.java
+++ Snakegame.java
@@ -1,1 +1,16 @@
+import java.util.LinkedList;
+import javax.swing.*;
+public class SnakeGame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Snake Game");
+        frame.add(new GamePanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+
+
+
 
